c2fb1cb4a43bc787c93febe9cbf2ab0b931c30a6
Tamas Czinege <tomi.czinege@gmail.com>
2014-03-07 23:55:21 +0000

    work in progress on switching sqlite library

--- src/sqlitedb.js
+++ src/sqlitedb.js
@@ -1,71 +1,81 @@
-// var sqlite3 = require('sqlite3');
-// var Q = require('q');
-// 
-// function withSqlite(fn) {
-// 	var deferred = Q.defer();
-// 	var db = new sqlite3.Database("db.sqlite");
-// 	Q
-// 		.fcall(function() {
-// 			return fn(db);
-// 		})
-// 		.then(function(res) {
-// 			deferred.resolve(res);
-// 		}, function(err) {
-// 			deferred.reject(err);
-// 		})
-// 		.fin(function() {
-// 			db.close();
-// 		});
-// 
-// 	return deferred.promise;
-// }
-// 
-// function begin(db) {
-// 	console.log('begin');
-// 	db.run('begin transaction');
-// }
-// 
-// function commit(db) {
-// 	console.log('commit');
-// 	db.run('commit transaction');
-// }
-// 
-// function createQueriesTable(db) {
-// 	db.run(
-// 		'create table if not exists query(' +
-// 		'id integer not null primary key autoincrement, ' +
-// 		'query text not null unique, ' +
-// 		'count integer not null default 0, ' +
-// 		'date_added datetime not null default current_timestamp)');
-// }
+var sqlite = require('sqlite-fts');
+var Q = require('q');
 
-function logQueries(queries) {
-	// return withSqlite(function(db) {
-	// 	db.serialize(function() {
-	// 		begin(db);
-	// 		createQueriesTable(db);
-	// 		logQueriesInDb(queries, db);
-	// 		commit(db);
-	// 	});
-	// });
+function withSqlite(fn) {
+	var deferred = Q.defer();
+	var db = new sqlite.Database("db.sqlite");
+	Q
+		.ninvoke(db, 'open')
+		.then(function() {
+			return fn(db);
+		})
+		.then(function(res) {
+			deferred.resolve(res);
+		}, function(err) {
+			deferred.reject(err);
+		})
+		.fin(function() {
+			db.close();
+		});
+
+	return deferred.promise;
 }
 
-// function logQueriesInDb(queries, db) {
-// 	queries.forEach(function(query) {
-// 		logQuery(query, db);
-// 	});
-// }
-// 
-// function logQuery(query, db) {
-// 	db.all('select 1 from query where query = ?', query.query, function(err, rows) {
-// 		if (err !== null) throw err;
-// 		if (rows.length === 1) {
-// 			db.run('update query set count = count + 1 where query = ?', query.query);
-// 		} else {
-// 			db.run('insert into query (query) values (?)', query.query);
-// 		}
-// 	});
-// }
+function begin(db) {
+	return Q.ninvoke(db, 'execute', 'begin transaction');
+}
+
+function commit(db) {
+	return Q.ninvoke(db, 'execute', 'commit transaction');
+}
+
+
+function createQueriesTable(db) {
+	return Q.ninvoke(
+		db,
+		'execute',
+		'create table if not exists query(' +
+		'id integer not null primary key autoincrement, ' +
+		'query text not null unique, ' +
+		'count integer not null default 0, ' +
+		'date_added datetime not null default current_timestamp)');
+}
+
+function logQueries(queries) {
+	return withSqlite(function(db) {
+		return Q
+			.fcall(function() {
+				return begin(db);
+			})
+			.then(function() {
+				return createQueriesTable(db);
+			})
+			.then(function() {
+				return logQueriesInDb(queries, db);
+			})
+			.then(function() {
+				return commit(db);
+			});
+	});
+}
+
+function logQueriesInDb(queries, db) {
+	queries.forEach(function(query) {
+		logQuery(query, db);
+	});
+}
+
+function logQuery(query, db) {
+	return Q
+		.ninvoke(db, 'execute', 'select 1 from query where query = ?', [query.query])
+		.then(function(rows) {
+			if (rows.length === 1) {
+				return Q.ninvoke(db, 'execute', 'update query set count = count + 1 where query = ?', [query.query]);
+			} else {
+				return Q.ninvoke(db, 'execute', 'insert into query (query) values (?)', [query.query]);
+			}
+		});
+}
 
 module.exports = {
 	logQueries: logQueries
